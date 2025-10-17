2d55098c2a9a9229288353a77224f1fdc9ce3d87
seva <sevakunjadas.bms@gmail.com>
2025-10-17 19:05:39 -0300

    TODO:dialog feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
+const uuid = crypto.randomUUID();
 
-function Books(name, author, pages, read) {
+function Books(name, author, pages, id) {
     if(!new.target) {
         throw Error("ponele new siome");
     }
@@ -8,25 +9,27 @@ function Books(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.id = id;
 }
 
 function addBookToLibrary() {
-    const book = new Books("lotr", "JRR Tolkien", 1216, "not read yet");
-    const book1 = new Books("100 anios de soledad", "marquez", 1234, "read");
-    myLibrary.push(book, book1)
-    // console.log(book);
+    let book = new Books (prompt("libro?: " + uuid));
+    // let book1 = prompt("otro?" + uuid);
+    // const book = new Books("lotr", "JRR Tolkien", 1216, "not read yet");
+    // const book1 = new Books("100 anios de soledad", "marquez", 1234, "read");
+    myLibrary.push(book);
+    console.log(` ${this.name}  ${this.id} `);
+    // console.log(` ${book1} ${uuid} id number`);
 }
 
 function showBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary[i]);
     }
-}
-
+};
 
 addBookToLibrary();
-showBooks()
 
-// console.log(addBookToLibrary());
-// console.log(showBooks());
+
+
+
