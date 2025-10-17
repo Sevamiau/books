f4b5171203b5809324d9228859079a85343d9145
seva <sevakunjadas.bms@gmail.com>
2025-10-17 17:25:24 -0300

    arrays workin

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
+function Books(name, author, pages, read) {
     if(!new.target) {
         throw Error("ponele new siome");
     }
@@ -12,5 +12,21 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary() {
-
+    const book = new Books("lotr", "JRR Tolkien", 1216, "not read yet");
+    const book1 = new Books("100 anios de soledad", "marquez", 1234, "read");
+    myLibrary.push(book, book1)
+    // console.log(book);
 }
+
+function showBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+    }
+}
+
+
+addBookToLibrary();
+showBooks()
+
+// console.log(addBookToLibrary());
+// console.log(showBooks());
