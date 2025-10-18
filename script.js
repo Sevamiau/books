987470e2e16a35d96b38fc296581b1f42f2d16e5
seva <sevakunjadas.bms@gmail.com>
2025-10-18 13:59:00 -0300

    feat:btn adding books

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const uuid = crypto.randomUUID();
+const idBtn = document.querySelector("#id-btn")
 
 function Books(name, author, pages, id) {
     if(!new.target) {
@@ -12,24 +13,27 @@ function Books(name, author, pages, id) {
     this.id = id;
 }
 
-function addBookToLibrary() {
-    let book = new Books (prompt("libro?: " + uuid));
-    // let book1 = prompt("otro?" + uuid);
-    // const book = new Books("lotr", "JRR Tolkien", 1216, "not read yet");
-    // const book1 = new Books("100 anios de soledad", "marquez", 1234, "read");
-    myLibrary.push(book);
-    console.log(` ${this.name}  ${this.id} `);
-    // console.log(` ${book1} ${uuid} id number`);
+
+// function showBooks() {
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         console.table(myLibrary[i]);
+//     }
+// };
+
+
+idBtn.addEventListener("click", 
+    function addBookToLibrary() {
+        let book = new Books (prompt("nombre?: "), 
+            prompt("autor?"), 
+            prompt("paginas?"), uuid);
+        myLibrary.push(book);
+        console.table(myLibrary);
 }
+);
 
-function showBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i]);
-    }
-};
+// addBookToLibrary();
+// showBooks(); 
 
-addBookToLibrary();
-
-
+console.table(myLibrary);
 
 
