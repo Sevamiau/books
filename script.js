f6d6cf2bbbec65e52aa114c69b813c8d1ac05e6d
seva <sevakunjadas.bms@gmail.com>
2025-10-18 23:23:40 -0300

    TODO:display object

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const myLibrary = ["dasd", "adasd"];
+const myLibrary = [];
 const showLibraryButton = document.querySelector("#show-library-button");
 const bookForm = document.getElementById("form-info"); 
 const addBookFormButton = bookForm.querySelector("button[type='button']"); 
 const dialogElem = document.getElementById("dialog");
 const showButton = document.querySelector(".show");
 const closeButton = document.querySelector(".close");
-const libraryDisplay = document.querySelector("#library-display")
+const libraryDisplay = document.querySelector("#library-display");
 
 
 showButton.addEventListener("click", () => {
@@ -26,16 +26,22 @@ function Books(name, author, pages, read, id) {
     this.pages = pages;
     this.read = read;
     this.id = id;
-}
+ }
+
 
 showLibraryButton.addEventListener("click", () => {
     for (let i =0; i < myLibrary.length; i++) {
-        const listLibrary = document.createElement("li");
+        const listLibrary = document.createElement("div");
         listLibrary.textContent = myLibrary[i];
         libraryDisplay.appendChild(listLibrary);
     }
+    for (let key in myLibrary) {
+        console.log(key);
+        console.log(myLibrary[key])
+    }
     console.table(myLibrary);
 });
+
 
 addBookFormButton.addEventListener("click", submitForm); 
 
