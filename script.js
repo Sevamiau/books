81525a979943fbabd0952f5801e8f1f61d3350a3
seva <sevakunjadas.bms@gmail.com>
2025-10-21 15:17:59 -0300

    TODO: function shows new books in cards just once

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, read: true },
+    { title: "Pride and Prejudice", author: "Jane Austen", pages: 432, read: false },
+    { title: "1984", author: "George Orwell", pages: 328, read: true }
+];
 const showLibraryButton = document.querySelector("#show-library-button");
 const bookForm = document.getElementById("form-info"); 
 const addBookFormButton = bookForm.querySelector("button[type='button']"); 
@@ -20,9 +24,9 @@ function Books(name, author, pages, read, id) {
     this.id = id;
 }
 
-addBookFormButton.addEventListener("click", submitForm); 
+addBookFormButton.addEventListener("click", addBookToLibrary); 
 
-function submitForm() {
+function addBookToLibrary() {
     const formData = new FormData(bookForm);
     
     const name = formData.get('book-name');
@@ -38,37 +42,98 @@ function submitForm() {
     bookForm.reset();
 }
 
-create.addEventListener("click", () => {
-    myLibrary.forEach(book => {
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("book-card");
 
-        const titlePara = document.createElement("p");
-        titlePara.textContent = `Title: ${book.title}`;
+// create.addEventListener("click", () => {
 
-        const authorPara = document.createElement("p");
-        authorPara.textContent = `Author ${book.author}`
+//     myLibrary.forEach(book => {
+//         const bookCard = document.createElement("div");
+//         bookCard.classList.add("book-card");
 
-        const pagesPara = document.createElement("p");
-        pagesPara.textContent = `pages ${book.pages}`;
+//         const titlePara = document.createElement("p");
+//         titlePara.textContent = `Name: ${book.name}`;
 
-        const readStatusPara = document.createElement("p");
-        readStatusPara.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+//         const authorPara = document.createElement("p");
+//         authorPara.textContent = `Author ${book.author}`
 
-        bookCard.appendChild(titlePara);
-        bookCard.appendChild(authorPara);
-        bookCard.appendChild(pagesPara);
-        bookCard.appendChild(readStatusPara);
+//         const pagesPara = document.createElement("p");
+//         pagesPara.textContent = `pages ${book.pages}`;
 
-        const currentDiv = document.getElementById("divperro1");
+//         const readStatusPara = document.createElement("p");
+//         readStatusPara.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
 
-        if (currentDiv) {
-            document.body.insertBefore(bookCard, currentDiv);
-        } else {
-            document.body.appendChild(bookCard);
-        }
-    })
-} );
+//         const idNumber = document.createElement("p");
+//         idNumber.textContent = `id Number ${book.id}`;
+
+//         bookCard.appendChild(titlePara);
+//         bookCard.appendChild(authorPara);
+//         bookCard.appendChild(pagesPara);
+//         bookCard.appendChild(readStatusPara);
+//         bookCard.appendChild(idNumber);
+
+//         const currentDiv = document.getElementById("divperro1");
+
+//         if (currentDiv) {
+//             document.body.insertBefore(bookCard, currentDiv);
+//         } else {
+//             document.body.appendChild(bookCard);
+//         }
+//     })
+// } );
+
+function addBookToLibrary() {
+    const formData = new FormData(bookForm);
+    
+    const name = formData.get('book-name');
+    const author = formData.get('author');
+    const pages = parseInt(formData.get('pages'));
+    const readValue = formData.get('read'); 
+    const read = (readValue === 'leido');
+    const uuid = crypto.randomUUID();
+
+    let book = new Books(name, author, pages, read, uuid);
+    myLibrary.push(book);
+
+    bookForm.reset();
+}
+
+
+
+
+myLibrary.forEach(book => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const titlePara = document.createElement("p");
+    titlePara.textContent = `Name: ${book.name}`;
+
+    const authorPara = document.createElement("p");
+    authorPara.textContent = `Author ${book.author}`
+
+    const pagesPara = document.createElement("p");
+    pagesPara.textContent = `pages ${book.pages}`;
+
+    const readStatusPara = document.createElement("p");
+    readStatusPara.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+
+    const idNumber = document.createElement("p");
+    idNumber.textContent = `id Number ${book.id}`;
+
+    bookCard.appendChild(titlePara);
+    bookCard.appendChild(authorPara);
+    bookCard.appendChild(pagesPara);
+    bookCard.appendChild(readStatusPara);
+    bookCard.appendChild(idNumber);
+
+    const currentDiv = document.getElementById("divperro1");
+
+    if (currentDiv) {
+        document.body.insertBefore(bookCard, currentDiv);
+    } else {
+        document.body.appendChild(bookCard);
+    }
+})
+
+
 
 
 showLibraryButton.addEventListener("click", () => {
